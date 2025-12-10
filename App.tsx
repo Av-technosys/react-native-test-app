0901dd96235cd180f27049185b17827241200967
Ashish Bishnoi <bishnoi11011@gmail.com>
2025-12-10 11:15:49 +0530

    project-demo

--- App.tsx
+++ App.tsx
@@ -6,11 +6,15 @@
  */
 
 import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
 import {
   SafeAreaProvider,
   useSafeAreaInsets,
 } from 'react-native-safe-area-context';
+import './global.css';
+
+import { CustomFlatList } from './src';
+
 
 function App() {
   const isDarkMode = useColorScheme() === 'dark';
@@ -27,11 +31,9 @@ function AppContent() {
   const safeAreaInsets = useSafeAreaInsets();
 
   return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
+    <View className="flex-1 items-center border border-red-500 justify-center bg-blue-500">
+      <Text className="text-white text-xl">Hello TailwindCSS 👋</Text>
+      <View className=' h-fit border border-red-500'><CustomFlatList items={list} /></View>
     </View>
   );
 }
@@ -43,3 +45,19 @@ const styles = StyleSheet.create({
 });
 
 export default App;
+
+
+const list = [
+  "first",
+  "second",
+  "third",
+  "first",
+  "second",
+  "third",
+  "first",
+  "second",
+  "third",
+  "first",
+  "second",
+  "third"
+]
--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,20 @@
-const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+// const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
+// /**
+//  * Metro configuration
+//  * https://reactnative.dev/docs/metro
+//  *
+//  * @type {import('@react-native/metro-config').MetroConfig}
+//  */
+// const config = {};
+
+// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { withNativeWind } = require('nativewind/metro');
+const defaultConfig = getDefaultConfig(__dirname);
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = withNativeWind(mergeConfig(defaultConfig, config), {
+  input: './global.css',
+});
