0eb57f17655d07ad5725811ac2d0505ac373f82f
Ashish Bishnoi <bishnoi11011@gmail.com>
2025-12-15 15:44:34 +0530

    home page category ui fix

--- src/components/HomePage/HomeCategoriesSection.tsx
+++ src/components/HomePage/HomeCategoriesSection.tsx
@@ -1,11 +1,5 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import { View, Text, Pressable } from 'react-native';
+import { View, Text, FlatList } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
-import { useNavigation, NavigationProp } from '@react-navigation/native';
-
-type RootStackParamList = {
-  Categories: any;
-};
 
 const categories = [
   { id: '1', title: 'Bar Tender', iconName: 'bar-chart-2' },
@@ -16,31 +10,17 @@ const categories = [
   { id: '6', title: 'Music', iconName: 'music' },
   { id: '7', title: 'Lightning', iconName: 'zap' },
   { id: '8', title: 'Kids Activity', iconName: 'users' },
-  { id: '9', title: 'Face Painting', iconName: 'smile' },
-  { id: '10', title: 'Magicians', iconName: 'wand-2' }
 ];
 
 export default function HomeCategoriesSection() {
-  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
-
   return (
     <View className="mt-3 px-4">
-      <View className="mt-6 flex-row flex-wrap justify-between">
-        {categories.map(item => (
-          <Pressable
-            key={item.id}
-            className="w-[19%] mb-6 items-center "
-            onPress={() => navigation.navigate('Categories')}
-          >
-            <View className="w-20 h-20 rounded-full bg-orange-50 items-center justify-center">
-              <Icon name={item.iconName} size={28} color="#ff6b35" />
-            </View>
-
-            <Text className="mt-2 text-[10px] text-center text-gray-700">
-              {item.title}
-            </Text>
-          </Pressable>
-        ))}
+      <View className="mt-6">
+        <FlatList
+          data={categories}
+          numColumns={4}
+          renderItem={({ item }) => <EventItem item={item} />}
+        />
       </View>
     </View>
   );
